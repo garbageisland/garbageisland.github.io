5bff51c269e7bc311c0fb64b333744e59f9b98fa
garbageisland <justinmseibert@gmail.com>
2016-12-26 17:25:58 -0800

    all questions

--- questions.js
+++ questions.js
@@ -1,5 +1,27 @@
 var order = [
-  // 'steve','bugs'
+  'home',
+  'plant',
+  'pattern',
+  'social',
+  'career',
+  'phenomena',
+  'worst',
+  'game',
+  'cookie',
+  'election',
+  'mannequin',
+  'fruit',
+  'bugs',
+  'code',
+  'steve',
+  'crowd',
+  'poo',
+  'riot',
+  'protest',
+  'tourism',
+  'celebs',
+  'staircase',
+  'spirit',
 ];
 
 var questions = {
@@ -9,32 +31,32 @@ var questions = {
       'a' : {
         'content': 'steve5.jpg',
         'points' : {
-            'default'  : 0,
-            'red' : 1,
-            'sharp' : 2,
+          'default'  : 0,
+          'red' : 1,
+          'sharp' : 2,
         },
       },
       'b' : {
         'content': 'steve8.png',
         'points' : {
-            'default' : 0,
-            'yellow'  : 1,
+          'default' : 0,
+          'yellow'  : 1,
         },
       },
       'c' : {
         'content': 'steve3.jpg',
         'points' : {
-            'default' : 0,
-            'black' : 1,
-            'red' : 1,
-            'sharp' : 1,
+          'default' : 0,
+          'black' : 1,
+          'red' : 1,
+          'sharp' : 1,
         },
       },
       'd' : {
         'content': 'steve12.jpg',
         'points' : {
-            'default' : 0,
-            'blue' : 1,
+          'default' : 0,
+          'blue' : 1,
         },
       },
     },
@@ -45,27 +67,27 @@ var questions = {
       'a' : {
         'content': 'bug14.jpg',
         'points' : {
-            'default' : 0,
-            'white' : 1,
+          'default' : 0,
+          'white' : 1,
         },
       },
       'b' : {
         'content': 'bug15.jpg',
         'points' : {
-            'default' : 0,
-            'red' : 1,
+          'default' : 0,
+          'red' : 1,
         },
       },
       'c' : {
         'content': 'bug18.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'bug3.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -76,25 +98,25 @@ var questions = {
       'a' : {
         'content': 'free4.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'b' : {
         'content': 'free1.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'free2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'free5.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -105,25 +127,25 @@ var questions = {
       'a' : {
         'content': 'protest1.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'b' : {
         'content': 'protest6.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'protest4.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'protest7.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -140,20 +162,20 @@ var questions = {
       'b' : {
         'content': 'cookie5.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'cookie3.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'cookie4.jpg',
         'points' : {
-            'default' : 0,
-            'yellow' : 1,
+          'default' : 0,
+          'yellow' : 1,
         },
       },
     },
@@ -170,19 +192,19 @@ var questions = {
       'b' : {
         'content': 'fruit2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'fruit14.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'fruit13.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -199,19 +221,19 @@ var questions = {
       'b' : {
         'content': 'worst2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'worst3.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'worst.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -228,19 +250,19 @@ var questions = {
       'b' : {
         'content': 'lifehack1.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'lifehack5.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'lifehack6.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -257,19 +279,19 @@ var questions = {
       'b' : {
         'content': 'poo2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'poo3.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'poo4.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -286,19 +308,19 @@ var questions = {
       'b' : {
         'content': 'celeb10.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'celeb9.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'celeb6.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -315,19 +337,19 @@ var questions = {
       'b' : {
         'content': 'crowd9.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'crowd10.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'crowd8.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -350,13 +372,13 @@ var questions = {
       'c' : {
         'content': 'protest9.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'riot2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
@@ -373,252 +395,339 @@ var questions = {
       'b' : {
         'content': 'election2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'election3.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'election4.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
   },
-    'game' : {
+  'game' : {
     'question': 'which game show attracts the most annoying contestants?',
     'answers' : {
       'a' : {
         'content': 'game1.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'b' : {
         'content': 'game5.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'game2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'game4.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
   },
-              'career' : {
-    'question': 'when you were a kid you wanted to grow up to be:',
+  'career' : {
+    'question': 'what do you want to be when you grow up?',
     'answers' : {
       'a' : {
         'content': 'career8.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'b' : {
         'content': 'career5.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'career11.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'career10.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
   },
-              'home' : {
+  'home' : {
     'question': 'choose your new home:',
     'answers' : {
       'a' : {
         'content': 'home1.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'b' : {
         'content': 'home2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'home3.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'home4.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
 
         },
       },
     },
   },
-              'mannequin' : {
+  'mannequin' : {
     'question': 'How did you feel the day after the election?',
     'answers' : {
       'a' : {
         'content': 'mannequin.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'b' : {
         'content': 'mannequin5.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'mannequin3.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'mannequin4.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
     },
   },
-              'pattern' : {
+  'pattern' : {
     'question': 'Pick a pattern.',
     'answers' : {
       'a' : {
         'content': 'pattern1.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'b' : {
         'content': 'pattern2.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'c' : {
         'content': 'pattern3.jpg',
         'points' : {
-            'default' : 0,
+          'default' : 0,
         },
       },
       'd' : {
         'content': 'pattern4.jpg',
         'points' : {
-            'default' : 0,
-          },
+          'default' : 0,
         },
       },
     },
-    'phenomena' : {
-      'question': 'Which phenomena do you want to experience?',
-      'answers' : {
-        'a' : {
-          'content': 'phenomena.jpg',
-          'points' : {
-              'default' : 0,
-          },
-        },
-        'b' : {
-          'content': 'phenomena2.jpg',
-          'points' : {
-              'default' : 0,
-          },
-        },
-        'c' : {
-          'content': 'phenomena3.jpg',
-          'points' : {
-              'default' : 0,
-          },
-        },
-        'd' : {
-          'content': 'phenomena4.jpg',
-          'points' : {
-              'default' : 0,
-            },
-          },
+  },
+  'phenomena' : {
+    'question': 'Which phenomena do you want to experience?',
+    'answers' : {
+      'a' : {
+        'content': 'phenomena6.jpg',
+        'points' : {
+          'default' : 0,
         },
       },
-      'plant' : {
-        'question': 'Pick a house plant.',
-        'answers' : {
-          'a' : {
-            'content': 'plant1.jpg',
-            'points' : {
-                'default' : 0,
-            },
-          },
-          'b' : {
-            'content': 'plant2.jpg',
-            'points' : {
-                'default' : 0,
-            },
-          },
-          'c' : {
-            'content': 'plant3.jpg',
-            'points' : {
-                'default' : 0,
-            },
-          },
-          'd' : {
-            'content': 'plant4.jpg',
-            'points' : {
-                'default' : 0,
-              },
-            },
-          },
+      'b' : {
+        'content': 'phenomena2.jpg',
+        'points' : {
+          'default' : 0,
         },
-        'staircase' : {
-          'question': 'This staircase leads to heaven:',
-          'answers' : {
-            'a' : {
-              'content': 'staircase1.jpg',
-              'points' : {
-                  'default' : 0,
-              },
-            },
-            'b' : {
-              'content': 'staircase2.jpg',
-              'points' : {
-                  'default' : 0,
-              },
-            },
-            'c' : {
-              'content': 'staircase3.jpg',
-              'points' : {
-                  'default' : 0,
-              },
-            },
-            'd' : {
-              'content': 'staircase4.jpg',
-              'points' : {
-                  'default' : 0,
+      },
+      'c' : {
+        'content': 'phenomena3.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'phenomena7.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+    },
+  },
+  'plant' : {
+    'question': 'Pick a house plant.',
+    'answers' : {
+      'a' : {
+        'content': 'plant1.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'b' : {
+        'content': 'plant2.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'plant3.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'plant4.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+    },
+  },
+  'staircase' : {
+    'question': 'which staircase will lead you to the afterlife?',
+    'answers' : {
+      'a' : {
+        'content': 'staircase1.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'b' : {
+        'content': 'staircase2.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'staircase3.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'staircase4.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+    },
+  },
+  'code' : {
+    'question': 'which language do you want to learn?',
+    'answers' : {
+      'a' : {
+        'content': 'code3.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'b' : {
+        'content': 'code2.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'code4.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'code1.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+    },
+  },
+  'spirit' : {
+    'question': 'which is your spirit animal?',
+    'answers' : {
+      'a' : {
+        'content': 'animal1.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'b' : {
+        'content': 'animal2.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'animal3.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'animal4.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+    },
+  },
+  'social' : {
+    'question': 'describe your presence on social media',
+    'answers' : {
+      'a' : {
+        'content': 'social5.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'b' : {
+        'content': 'social4.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'social3.jpg',
+        'points' : {
+          'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'social1.jpg',
+        'points' : {
+          'default' : 0,
         },
       },
     },
